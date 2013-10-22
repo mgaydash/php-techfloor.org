ac83c507d3072c31214194c28b7adb00a5685ac8
mgaydash <mgaydash@gmail.com>
2013-10-22 16:40:24 -0400

    Update lol page 2013

--- view/lolTournament.php
+++ view/lolTournament.php
@@ -16,39 +16,36 @@ include("../frag/header.php");
 	?>
 </div>
 <div class="pageTitle">
-    LoL ARAM Tournament
+    LoL Tournament
 </div>
-<div style="border:2px solid black; border-top:none; height:200px; margin-left:70px; width:600px;">
-	<img alt="LoL Logo" src="../images/lolLogo.png"/>
+<div style="border:2px solid black; border-top:none; display:inline-block; margin-left:70px;">
+	<img alt="LoL Logo" src="../images/lolLogo2.png" style="display:block;">
 </div>
 <span class="mMenuButton openSighupForm" style="display:block;">Sign Up</span>
 <div class="textFormat">
 	<span style="line-height:22px;">
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		On Saturday, November 10, 2012, TechFloor will be hosting a League of Legends Tournament.
-		The tournament will be a series of 5v5 ARAM (all random, all mid) matches on the Proving Grounds.
+		On Saturday, November 2, 2013, TechFloor will be hosting a League of Legends Tournament.
+		The tournament will be a series of 5v5 team matches.
 		Teams can be made in advance by having all team mates specify the same team name on the sign-up form.
 		If you don't have a full five-person team, don't worry, teams will be made the day of the tournament
 		for those without a pre-made.
 		<br/>
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		The first match of the tournament will start at noon, and the tournament will continue until all but one team is eliminated.
-		We will be holding the tournament in Becker room 200 (Advanced Lab), and spectating will be done in
-		Becker room 143. At the end of the tournament, the winning team will receive 50% of the collected
-		entrance fees as a prize.
+		We will be holding the tournament in Becker room 120. At the end of the tournament, winning teams will recieve RP as prizes.
 		<h3>About:</h3>
 		<ul>
-			<li>Date: Saturday, November 10, 2012</li>
+			<li>Date: November 2, 2013</li>
 			<li>Time: 12:00pm</li>
-			<li>Where: Becker Advanced Lab - room 200</li>
+			<li>Where: Becker Advanced Lab - room 120</li>
 			<li>$5 participation fee for players</li>
 			<li>Single-elimination</li>
-			<li>5v5 ARAM's on the Proving Grounds</li>
 		</ul>
-		<h3>Rules:</h3>
+		<!-- <h3>Rules:</h3>
 		<ul>
 			<li>Queue dodging is forbidden.</li>
-		</ul>
+		</ul> -->
 	</span>
 </div>
 <!--<span class="mMenuButton openSighupForm" style="display:block;">Sign Up</span>-->
